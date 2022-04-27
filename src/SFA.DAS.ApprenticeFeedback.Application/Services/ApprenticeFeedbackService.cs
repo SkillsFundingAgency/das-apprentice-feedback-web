5838218b9466ec9c3e26def503b8c535ff1fff1f
Matt <matt.daniels@digital.education.gov.uk>
2022-04-27 15:20:19 +0100

    Sync up the FeedbackEligibility enum values

--- src/SFA.DAS.ApprenticeFeedback.Application/Services/ApprenticeFeedbackService.cs
+++ src/SFA.DAS.ApprenticeFeedback.Application/Services/ApprenticeFeedbackService.cs
@@ -20,97 +20,26 @@ namespace SFA.DAS.ApprenticeFeedback.Application.Services
 
         public async Task<IEnumerable<Domain.Models.Feedback.TrainingProvider>> GetTrainingProviders(Guid apprenticeId)
         {
-            // Get the training providers from the Outer Api
-            //var response = await _apiClient.GetTrainingProviders(apprenticeId);
-
-            var response = new GetTrainingProvidersResponse()
+            try
             {
-                InitialDenyPeriodDays = 92,
-                FinalAllowedPeriodDays = 92,
-                RecentDenyPeriodDays = 14,
-                TrainingProviders = new List<Domain.Api.Responses.TrainingProvider>() {
-                    /*
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000001,
-                        ProviderName = "Test Provider Started Too Recently",
-                        FeedbackEligibility = Domain.Api.Responses.FeedbackEligibility.Deny_TooSoon,
-                        TimeWindow = new TimeSpan(days: 92, hours: 0, minutes: 0, seconds: 0),
-                        SignificantDate = DateTime.UtcNow.AddDays(92)
-                    },
-                    */
-                    
-                    /*
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000002,
-                        ProviderName = "Test Provider Passed Too Long Ago",
-                        FeedbackEligibility = Domain.Api.Responses.FeedbackEligibility.Deny_TooLateAfterPassing,
-                        TimeWindow = new TimeSpan(days: 92, hours: 0, minutes: 0, seconds: 0),
-                    },
-                    */
+                // Get the training providers from the Outer Api
+                var response = await _apiClient.GetTrainingProviders(apprenticeId);
 
-                    /*
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000003,
-                        ProviderName = "Test Provider Withdrew Too Long Ago",
-                        FeedbackEligibility = Domain.Api.Responses.FeedbackEligibility.Deny_TooLateAfterWithdrawing,
-                        TimeWindow = new TimeSpan(days: 92, hours: 0, minutes: 0, seconds: 0),
-                    },
-                    */
-
-                    /*
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000004,
-                        ProviderName = "Test Provider Has Given Feedback Recently",
-                        FeedbackEligibility= Domain.Api.Responses.FeedbackEligibility.Deny_HasGivenFeedbackRecently,
-                        DateSubmitted = DateTime.UtcNow.AddDays(-10),
-                        SignificantDate = DateTime.UtcNow.AddDays(4)
-                    },
-                    */
-
-                    /*
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000005,
-                        ProviderName = "Test Provider Has Given Final Feedback",
-                        FeedbackEligibility = Domain.Api.Responses.FeedbackEligibility.Deny_HasGivenFinalFeedback,
-                        TimeWindow = new TimeSpan(days: 92, hours: 0, minutes: 0, seconds: 0),
-                    },
-                    */
-
-                    
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000006,
-                        ProviderName = "Test Provider Never Given Feedback",
-                        FeedbackEligibility = Domain.Api.Responses.FeedbackEligibility.Allow,
-                    },
-
-                    /*
-                    new Domain.Api.Responses.TrainingProvider
-                    {
-                        UkPrn = 100000007,
-                        ProviderName = "Test Provider Long Enough Since Previous Feedback",
-                        FeedbackEligibility = Domain.Api.Responses.FeedbackEligibility.Allow,
-                        DateSubmitted = DateTime.UtcNow.AddMonths(-4),
-                    }
-                    */
-                },
-            };
-
-            // automapper?
-            return response.TrainingProviders.Select(tp => new Domain.Models.Feedback.TrainingProvider()
+                // automapper?
+                return response.TrainingProviders.Select(tp => new Domain.Models.Feedback.TrainingProvider()
+                {
+                    Name = tp.ProviderName,
+                    Ukprn = tp.UkPrn,
+                    DateSubmitted = tp.LastFeedbackSubmittedDate,
+                    FeedbackEligibility = (Domain.Models.Feedback.FeedbackEligibility)tp.FeedbackEligibility,
+                    TimeWindow = tp.TimeWindow,
+                    SignificantDate = tp.SignificantDate,
+                });
+            }
+            catch(Exception ex)
             {
-                Name = tp.ProviderName,
-                Ukprn = tp.UkPrn,
-                DateSubmitted = tp.DateSubmitted,
-                FeedbackEligibility = (Domain.Models.Feedback.FeedbackEligibility)Enum.Parse(typeof(Domain.Models.Feedback.FeedbackEligibility), tp.FeedbackEligibility.ToString()),
-                TimeWindow = tp.TimeWindow,
-                SignificantDate = tp.SignificantDate,
-            });
+                throw;
+            }
         }
 
         public async Task<Domain.Models.Feedback.TrainingProvider> GetTrainingProvider(Guid apprenticeId, long ukprn)
--- src/SFA.DAS.ApprenticeFeedback.Domain/Models/Feedback/FeedbackEligibility.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Models/Feedback/FeedbackEligibility.cs
@@ -2,11 +2,15 @@
 {
     public enum FeedbackEligibility
     {
-        Allow,
-        Deny_TooSoon,
-        Deny_TooLateAfterPassing,
-        Deny_TooLateAfterWithdrawing,
-        Deny_HasGivenFeedbackRecently,
-        Deny_HasGivenFinalFeedback
+        Unknown = 0,
+        Allow = 1,
+        Deny_TooSoon = 2,
+        Deny_TooLateAfterPassing = 3,
+        Deny_TooLateAfterWithdrawing = 4,
+        Deny_TooLateAfterPausing = 5,
+        Deny_HasGivenFeedbackRecently = 6,
+        Deny_HasGivenFinalFeedback = 7,
+        Deny_NotEnoughActiveApprentices = 8,
+        Deny_Complete = 9,
     }
 }
