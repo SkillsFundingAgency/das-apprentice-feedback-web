d84fdcd04fba303bb8f3f36360261097adeb20f2
Matt <matt.daniels@digital.education.gov.uk>
2022-05-20 11:14:00 +0100

    Unit test the landing page logic

--- src/SFA.DAS.ApprenticeFeedback.Web.UnitTests/Pages/WhenRequestingIndexPage.cs
+++ src/SFA.DAS.ApprenticeFeedback.Web.UnitTests/Pages/WhenRequestingIndexPage.cs
@@ -1,114 +1,113 @@
-﻿//using Microsoft.Extensions.Logging;
-//using Moq;
-//using NUnit.Framework;
-//using SFA.DAS.ApprenticeFeedback.Domain.Interfaces;
-//using SFA.DAS.ApprenticeFeedback.Domain.Models.Feedback;
-//using SFA.DAS.ApprenticeFeedback.Infrastructure.Session;
-//using SFA.DAS.ApprenticeFeedback.Web.Pages;
-//using SFA.DAS.ApprenticePortal.SharedUi.Menu;
-//using System;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeFeedback.Domain.Interfaces;
+using SFA.DAS.ApprenticeFeedback.Domain.Models.Feedback;
+using SFA.DAS.ApprenticeFeedback.Infrastructure.Session;
+using SFA.DAS.ApprenticeFeedback.Web.Pages;
+using SFA.DAS.ApprenticeFeedback.Web.UnitTests.Helpers;
+using SFA.DAS.ApprenticePortal.Authentication;
+using SFA.DAS.ApprenticePortal.SharedUi.Menu;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace SFA.DAS.ApprenticeFeedback.Web.UnitTests.Pages
-//{
-//    public class WhenRequestingIndexPage
-//    {
-//        private IndexModel IndexPage;
+namespace SFA.DAS.ApprenticeFeedback.Web.UnitTests.Pages
+{
+    public class WhenRequestingIndexPage
+    {
+        private IndexModel IndexPage;
 
-//        private Mock<IApprenticeFeedbackService> _mockFeedbackService;
-//        private Mock<IApprenticeFeedbackSessionService> _mockSession;
-//        private NavigationUrlHelper _urlHelper;
+        private Mock<IApprenticeFeedbackService> _mockFeedbackService;
+        private Mock<IApprenticeFeedbackSessionService> _mockSession;
+        private NavigationUrlHelper _urlHelper;
+        private AuthenticatedUser _authenticatedUser;
 
-//        [SetUp]
-//        public void Arrange()
-//        {
-//            _mockFeedbackService = new Mock<IApprenticeFeedbackService>();
-//            _mockSession = new Mock<IApprenticeFeedbackSessionService>();
+        [SetUp]
+        public void Arrange()
+        {
+            _mockFeedbackService = new Mock<IApprenticeFeedbackService>();
+            _mockSession = new Mock<IApprenticeFeedbackSessionService>();
 
-//            var urls = new NavigationSectionUrls();
-//            urls.ApprenticeHomeUrl = new Uri("https://localhost:5001/");
+            var urls = new NavigationSectionUrls();
+            urls.ApprenticeHomeUrl = new Uri("https://localhost:5001/");
+            _urlHelper = new NavigationUrlHelper(urls);
 
-//            _urlHelper = new NavigationUrlHelper(urls);
+            _authenticatedUser = AuthenticatedUserHelper.CreateAuthenticatedUser(Guid.NewGuid());
 
-//            IndexPage = new IndexModel(_mockFeedbackService.Object, _mockSession.Object, _urlHelper, Mock.Of<ILogger<IndexModel>>());
-//        }
+            IndexPage = new IndexModel(Mock.Of<ILogger<IndexModel>>(), _mockFeedbackService.Object, _urlHelper, _mockSession.Object);
+        }
 
-//        public async Task And_MultipleTrainingProvidersAreReturned_Then_ShowIndexPage()
-//        {
+        [Test]
+        public async Task And_MultipleTrainingProvidersReturned_Then_ShowIndexPage()
+        {
+            _mockFeedbackService.Setup(m => m.GetTrainingProviders(It.IsAny<Guid>()))
+                .ReturnsAsync(new List<TrainingProvider>()
+                {
+                    new TrainingProvider()
+                    {
+                        Ukprn = 12345678,
+                        Name = "First Training Provider",
+                        FeedbackEligibility = FeedbackEligibility.Allow
+                    },
+                    new TrainingProvider()
+                    {
+                        Ukprn = 87654321,
+                        Name = "Second Training Provider",
+                        FeedbackEligibility = FeedbackEligibility.Allow
+                    }
+                });
 
-//        }
+            var result = await IndexPage.OnGet(_authenticatedUser);
 
-//        public async Task And_SingleProviderReturned_Then_RedirectToStartPage()
-//        {
-//            var response = new List<TrainingProvider>
-//            {
-//                GetTrainingProviderValidForFeedback()
-//            };
+            result.Should().BeOfType<PageResult>();
+        }
 
-//            var result = IndexPage.OnGet() 
-//        }
+        [Test]
+        public async Task And_SingleProviderReturned_And_FeedbackAllowed_Then_RedirectToStartPage()
+        {
+            _mockFeedbackService.Setup(m => m.GetTrainingProviders(It.IsAny<Guid>()))
+                .ReturnsAsync(new List<TrainingProvider>() 
+                { 
+                    new TrainingProvider() 
+                    { 
+                        Ukprn = 12345678,
+                        Name = "Single Training Provider",
+                        FeedbackEligibility = FeedbackEligibility.Allow
+                    }
+                });
 
-//        private TrainingProvider GetValidTrainingProvider()
-//        {
+            var result = await IndexPage.OnGet(_authenticatedUser);
 
-//        }
+            result.Should().BeOfType<RedirectResult>();
+            var pageResult = (RedirectResult)result;
 
-//        private List<TrainingProvider> GetMultipleTrainingProviders()
-//        {
-//            return new List<TrainingProvider>
-//            {
-//                GetTrainingProviderValidForFeedback(),
-//                new TrainingProvider
-//                {
-//                    Ukprn = 100000001,
-//                    Name = "Test provider 2",
-//                    Apprenticeships = new List<Apprenticeship>
-//                    {
-//                        new Apprenticeship
-//                        {
-//                            LarsCode = 1,
-//                            StartDate = DateTime.Now,
-//                            FeedbackCompletionDates = new List<DateTime>()
-//                        }
-//                    }
-//                },
-//                new TrainingProvider
-//                {
-//                    Ukprn = 100000001,
-//                    Name = "Test provider 1",
-//                    Apprenticeships = new List<Apprenticeship>
-//                    {
-//                        new Apprenticeship
-//                        {
-//                            LarsCode = 1,
-//                            StartDate = DateTime.Now.AddMonths(-12),
-//                            FeedbackCompletionDates = new List<DateTime>
-//                            {
-//                                DateTime.Now.AddDays(-5)
-//                            }
-//                        }
-//                    }
-//                }
-//            };
-//        }
+            pageResult.Url.Should().Be("/start/12345678");
+        }
 
-//        private TrainingProvider GetTrainingProviderValidForFeedback()
-//        {
-//            return new TrainingProvider
-//            {
-//                Ukprn = 100000003,
-//                Name = "Test provider 3",
-//                Apprenticeships = new List<Apprenticeship>
-//                    {
-//                        new Apprenticeship
-//                        {
-//                            LarsCode = 3,
-//                            StartDate = DateTime.Now.AddMonths(-4),
-//                            FeedbackCompletionDates = new List<DateTime>()
-//                        }
-//                    }
-//            };
-//        }
-//    }
-//}
+        [Test]
+        public async Task And_SingleProviderReturned_And_FeedbackNotAllowed_Then_RedirectToStatusPage()
+        {
+            _mockFeedbackService.Setup(m => m.GetTrainingProviders(It.IsAny<Guid>()))
+                .ReturnsAsync(new List<TrainingProvider>()
+                {
+                    new TrainingProvider()
+                    {
+                        Ukprn = 12345678,
+                        Name = "Single Training Provider",
+                        FeedbackEligibility = FeedbackEligibility.Deny_HasGivenFeedbackRecently
+                    }
+                });
+
+            var result = await IndexPage.OnGet(_authenticatedUser);
+
+            result.Should().BeOfType<RedirectResult>();
+            var pageResult = (RedirectResult)result;
+
+            pageResult.Url.Should().Be("/status");
+        }
+    }
+}
