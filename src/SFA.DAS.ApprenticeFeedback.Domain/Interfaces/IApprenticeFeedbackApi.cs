23cdf6a057fff15dc3c0886e29f65067f7630a3d
Matt <matt.daniels@digital.education.gov.uk>
2022-09-22 14:38:40 +0100

    Fix bug in RestEase API definition

--- src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IApprenticeFeedbackApi.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IApprenticeFeedbackApi.cs
@@ -24,7 +24,7 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Interfaces
         [Get("/apprentice/{id}")]
         Task<Apprentice> GetApprentice([Path] Guid id);
 
-        [Post("apprenticefeedback/{id}/exitsurvey")]
+        [Post("apprenticefeedback/exitsurvey")]
         Task SubmitExitSurvey([Body] PostSubmitExitSurvey exitSurvey);
     }
 }
